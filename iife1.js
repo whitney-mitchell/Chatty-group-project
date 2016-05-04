bf8e3914b2da316abe15ad4fc4d43824db095038
Glenn Spinner <glenngineer1@gmail.com>
2016-05-04 10:29:55 -0500

    array of objects making it to console, not DOM

--- iife1.js
+++ iife1.js
@@ -2,19 +2,26 @@
 //load JSON file and return the array of objects. . AHA! IT IS ONJECTS!
 var chatty = (function(newchatty) {
 
-}(chatty));
-
-  function executeThisCodeIfFileFails () {
+function executeThisCodeIfFileFails () {
   // console.log("an error occurred while loading this file");
 }
 
-  function executeThisCodeAfterFileIsLoaded () {
+function executeThisCodeAfterFileIsLoaded () {
   // console.log(this.responseText);
+
+var data = JSON.parse(this.responseText);
+  // console.log("data", data);
+
+var messageList = document.getElementsByClassName("messageContainer");
+
+for(currentMessages in data.messages) {
+  var messageData = "";
+  var jsonMessages = data.messages[currentMessages];
+  messageData +=jsonMessages.messages;
+
+  messageList.innerHTML += messageData;
+  }
 }
-
-  var data = JSON.parse(this.responseText);
-  console.log("data", data);
-
 
 var myRequest = new XMLHttpRequest();
 
@@ -24,3 +31,6 @@ myRequest.addEventListener("error", executeThisCodeIfFileFails);
 myRequest.open("get", "main.json");
 
 myRequest.send();
+
+}(chatty));
+
