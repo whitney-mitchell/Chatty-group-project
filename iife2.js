0c18f4772a42d1affd470681296f794be1db697b
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-05-04 14:05:46 -0500

    all changes, seemingly random, but all to make iife 2 work.

--- iife2.js
+++ iife2.js
@@ -3,26 +3,20 @@
 //expose a function to read ALL messages and delete THE SELECTED message.
 //below is example of setting query to body for adding elements.
 //
-var chatty = (function() {
+var chatty = (function(string) {
+
   var messages = [];
+  string.addMsg = function(userText) {
+    messages.push(userText);
 
-  return {
-    getMessagesArray: function() {
-      return messages;
-    }
-    addMsg: function(userText) {
-      messages.push(userText);
-      return messages;
-      var toDom = document.getElementById("messageCon");
-      // var toMessage = "";
-      // for (var i = 0; i < userText.length; i++) {
-      //   toMessage += `<div class="singleMessage">${userText[i]}</div>`;
+    var toDom = document.getElementById("messageCon");
 
-      toDom.innerHTML = messages;
-      }
-      return toDom;
-    }
-  };
+    toDom.innerHTML = messages;
+  }
+  string.getMessagesArray = function() {
+    return messages;
+  }
+   return string;
 })(chatty || {});
 
 
