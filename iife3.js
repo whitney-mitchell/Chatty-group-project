f3ace5d529f50833cd2f1146fa55363ff80b04a9
Michael Mead <michaelmead007@icloud.com>
2016-05-04 10:20:50 -0500

    function to delete from DOM, still needs method argument from IIFE2

--- iife3.js
+++ iife3.js
@@ -2,10 +2,14 @@
 //remove "correct" element from the DOM.
 //Remove corresponding message from the private array in IIFE2. the getter
 //
-(function(chatty) {
-var message =  document.getElementByID('messageCon').innerHTML;
-document.body.addEventListener("click", deletebutton);
-function(methodName) {
-  
-}
-}(chatty || {}));.
+
+var chatty = (function(chatty) {
+  var message =  document.getElementByID('messageContainer').innerHTML;
+  document.querySelector("body").addEventListener("click", function(event) {
+    if (event.target.class  === "deleteButton") {
+      //delete the corresponding message
+      this.message.setAttribute("disabled", true);
+
+    }
+  })
+}(chatty || {}));
