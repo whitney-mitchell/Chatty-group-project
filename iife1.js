a45c584fcd813a6e92208195801ea67738464141
Glenn Spinner <glenngineer1@gmail.com>
2016-05-04 09:25:23 -0500

    starting Wednesday

--- iife1.js
+++ iife1.js
@@ -1,8 +1,24 @@
-var chatty = (function(newchatty) {
+var chatty = (function(chatty1) {
 
 }(chatty));
 
-var myRequest = new HMLHttpRequest();
+  function executeThisCodeIfFileFails () {
+  // console.log("an error occurred while loading this file");
+}
+
+  function executeThisCodeAfterFileIsLoaded () {
+  // console.log(this.responseText);
+}
+
+  var data = JSON.parse(this.responseText);
+  console.log("data", data);
+
+
+var myRequest = new XMLHttpRequest();
 
 myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
 myRequest.addEventListener("error", executeThisCodeIfFileFails);
+
+myRequest.open("get", "main.json");
+
+myRequest.send();
