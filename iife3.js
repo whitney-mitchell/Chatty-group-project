d6eb757ab3354167a8a24caaa69919fa56b13933
Michael Mead <michaelmead007@icloud.com>
2016-05-04 09:53:01 -0500

    iife3 function to delete from DOM started

--- iife3.js
+++ iife3.js
@@ -1,3 +1,11 @@
-//Accept a message element ID 
+//Accept a message element ID 'messageCon'
 //remove "correct" element from the DOM.
-//Remove corresponding message from the private array in IIFE2. 
+//Remove corresponding message from the private array in IIFE2. the getter
+//
+(function(chatty) {
+var message =  document.getElementByID('messageCon').innerHTML;
+document.body.addEventListener("click", deletebutton);
+function(methodName) {
+  
+}
+}(chatty || {}));.
