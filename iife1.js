654ce71e0dae21d2c920076ccd7b04676fd7539e
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-05-04 15:22:22 -0500

    checking my files

--- iife1.js
+++ iife1.js
@@ -2,40 +2,40 @@
 //load JSON file and return the array of objects. . AHA! IT IS OBJECTS!
 var chatty = (function(newchatty) {
 
-function executeThisCodeIfFileFails () {
-  // console.log("an error occurred while loading this file");
-}
+	function executeThisCodeIfFileFails () {
+	  // console.log("an error occurred while loading this file");
+	}
 
-function executeThisCodeAfterFileIsLoaded () {
-  // console.log(this.responseText);
+	function executeThisCodeAfterFileIsLoaded () {
+	  // console.log(this.responseText);
 
-  var dataArray = JSON.parse(this.responseText).messages;
-    // console.log("data", dataArray);
+	  var dataArray = JSON.parse(this.responseText).messages;
+	    // console.log("data", dataArray);
 
-  var messageList = document.getElementById("messageCon");
+	  var messageList = document.getElementById("messageCon");
 
-  for(index in dataArray) {
-    var messageData = "";
-    var messageObject = dataArray[index];
-    for (key in messageObject) {
-      var output = messageObject[key];
-      messageData += "<div>";
-      messageData += output;
-      messageData += "</div>";
+	  for(index in dataArray) {
+	    var messageData = "";
+	    var messageObject = dataArray[index];
+	    for (key in messageObject) {
+	      var output = messageObject[key];
+	      messageData += "<div>";
+	      messageData += output;
+	      messageData += "</div>";
 
-      messageList.innerHTML += messageData;
-    }
-  }
-}
+	      messageList.innerHTML += messageData;
+	    }
+	  }
+	}
 
-var myRequest = new XMLHttpRequest();
+	var myRequest = new XMLHttpRequest();
 
-myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
-myRequest.addEventListener("error", executeThisCodeIfFileFails);
+	myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
+	myRequest.addEventListener("error", executeThisCodeIfFileFails);
 
-myRequest.open("get", "main.json");
+	myRequest.open("get", "main.json");
 
-myRequest.send();
-
-}(chatty));
+	myRequest.send();
+	return newchatty;
+})(chatty || {});
 
--- main.js
+++ main.js
@@ -13,3 +13,12 @@ function big(){
   console.log("bigger");
   document.querySelector("body").classList.toggle("largeText");
 }
+
+var msgResult = document.getElementById("messageInput");
+msgResult.addEventListener("keypress", function(event) {
+	if (event.keyCode === 13) {
+		console.log(msgResult.value);
+		chatty.addMsg(msgResult.value);
+		msgResult.value = "";
+	}
+});
